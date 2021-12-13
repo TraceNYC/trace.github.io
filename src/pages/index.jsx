2b5c5b5b11016e2c85cfbc0108ab730d1080b1b2
edwardkwon95 <66803062+edwardkwon95@users.noreply.github.com>
2021-12-13 15:58:53 -0500

    testing images

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,15 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { prefix } from '../utils/prefix'
 
 import Navbar from '../components/Navbar'
 
 const Home = () => {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <img src={`${prefix}/Dashboard-chart1.png`}/>
+    </>
   )
 }
 
