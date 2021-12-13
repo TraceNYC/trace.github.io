94dc0242e2da40d9d63ef651e662fd839d0dff1d
edwardkwon95 <66803062+edwardkwon95@users.noreply.github.com>
2021-12-13 16:37:12 -0500

    testing img loader

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,27 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import Image from 'next/image';
 import navStyles from '../styles/Navbar.module.css'
 import { Box, Flex, IconButton, useColorModeValue } from '@chakra-ui/react'
+import { 
+  AiFillGithub,
+  AiFillLinkedin
+} from "react-icons/ai";
+import { prefix } from '../utils/prefix';
 import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons'
 
+const logoLoader = ({ src, width, quality }) => {
+  return `https://tracenyc.github.io/trace.github.io/${src}?w=${width}&q=${quality || 75}`
+}
+
 const Navbar = () => {
+
   const Links = [
     {
-      name: "Home",
+      name: "HOME",
       path: "/",
     },
     {
-      name: "About",
+      name: "ABOUT",
       path: "/about",
     },
     {
@@ -18,28 +29,22 @@ const Navbar = () => {
       path: "/howTo/",
     }
   ];
+
   return (
     <div className="navStyles.navbar">
-      <Box>
+      <Flex
+        justifyContent="space-between"
+      >
         <Flex>
-          <IconButton
-            size="md"
-            icon={<HamburgerIcon />}
-          // aria-label="Open Menu"
-          // display={{ md: "None "}}
-          // onClick={isOpen ? onClose : onOpen}
-          />
-          <Box
-            px={2}
-            py={1}
-            rounded={"md"}
-            _hover={{
-              textDecoration: "none",
-              bg: useColorModeValue("gray.200", "gray.700"),
-            }}
-          >
-            <Link href="/">Home</Link>
-          </Box>
+          <Link href="/">
+            <Image 
+              loader={logoLoader} 
+              src={`/T.png`} 
+              alt='Trace Logo'
+              width={'50px'}
+              height={'50px'}
+            />
+          </Link>
           <Box
             px={2}
             py={1}
@@ -60,32 +65,22 @@ const Navbar = () => {
               bg: useColorModeValue("gray.200", "gray.700"),
             }}
           >
-            <Link href="/howTo">How To</Link>
-          </Box>
-          <Box
-            px={2}
-            py={1}
-            rounded={"md"}
-            _hover={{
-              textDecoration: "none",
-              bg: useColorModeValue("gray.200", "gray.700"),
-            }}
-          >
-            <Link href="https://github.com/oslabs-beta/trace">GitHub</Link>
-          </Box>
-          <Box
-            px={2}
-            py={1}
-            rounded={"md"}
-            _hover={{
-              textDecoration: "none",
-              bg: useColorModeValue("gray.200", "gray.700"),
-            }}
-          >
-            <Link href="/https://www.npmjs.com/package/@go-trace/tracer">npm</Link>
+            <Link href="/getting-started">Getting Started</Link>
           </Box>
         </Flex>
-      </Box>
+        <Flex>
+          <IconButton 
+            href='https://www.linkedin.com/company/go-trace'
+            aria-label='LinkedIn' 
+            icon={<AiFillLinkedin />}  
+          />
+          <IconButton 
+            href='https://github.com/oslabs-beta/trace'
+            aria-label='GitHub' 
+            icon={<AiFillGithub />}  
+          />
+        </Flex>
+      </Flex>
     </div>
   )
 }
