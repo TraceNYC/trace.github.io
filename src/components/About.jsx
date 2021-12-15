99f5ec76de614f4ba0b3a2ee88592eee4cc42ae5
edwardkwon95 <66803062+edwardkwon95@users.noreply.github.com>
2021-12-15 13:02:28 -0500

    apostrophe issue

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -37,7 +37,7 @@ const About = () => {
             What <br />is <br />Trace?
           </Heading>
           <Text w='50%' color='#fff' pr='8rem' pl='3rem' fontSize='1.4rem' lineHeight='1.7' textAlign='justify'>
-            Trace is a lightweight, open-source GraphQL query performance monitoring GUI that provides users with real-time, resolver-level performance tracing metrics and error logging. At its inception, Trace was created for the developer looking for a straightforward option for query performance monitoring on their Express-GraphQL server -- without having to install hefty libraries full of tools they'll never use such as Apollo or Hasura. Trace provides a simple, intuitive interface for users looking to test queries for their Express-GraphQL servers.
+            Trace is a lightweight, open-source GraphQL query performance monitoring GUI that provides users with real-time, resolver-level performance tracing metrics and error logging. At its inception, Trace was created for the developer looking for a straightforward option for query performance monitoring on their Express-GraphQL server -- without having to install hefty libraries full of tools they{"'"}ll never use such as Apollo or Hasura. Trace provides a simple, intuitive interface for users looking to test queries for their Express-GraphQL servers.
           </Text>
         </Flex>
 
